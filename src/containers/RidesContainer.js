b227cd65ef2b4e3eac2ef063439cac582d387d82
morgancollado <morgan.collado@gmail.com>
2021-03-15 20:51:39 -0500

    adds search bar to RidesContainer

--- src/containers/RidesContainer.js
+++ src/containers/RidesContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import MyRides from '../components/MyRides'
+import SearchBar from '../components/SearchBar'
 
 const RidesContainer = () => {
     return(
         <div className="RidesContainer">
             <MyRides/>
+            
         </div>
     )
 }
